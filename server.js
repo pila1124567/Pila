09e01aeee88c25f1bc05005e7da89671a9af5272
pila1124567 <8c8mpfgsbd@privaterelay.appleid.com>
2025-12-09 23:38:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require("express");
-const http = require("http");
-const socketio = require("socket.io");
 const path = require("path");
-
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
-io.on("connection", socket => {
-    console.log("a user connected");
-
-    socket.on("scanned", data => {
-        io.emit("showOnDesktop", data);
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "scan.html")); 
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Server running on port " + PORT));
