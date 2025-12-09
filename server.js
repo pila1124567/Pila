b9ed0cd89782ca16a943841ae0e37744c9463b01
pila1124567 <8c8mpfgsbd@privaterelay.appleid.com>
2025-12-10 00:07:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+// Default route → scan.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "scan.html")); 
+  res.sendFile(path.join(__dirname, "scan.html"));
 });
 
+// Render requires this to start on the right port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
