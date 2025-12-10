67cc35e5053c8d9d2fc2f32881d3a716cbd629ff
pila1124567 <8c8mpfgsbd@privaterelay.appleid.com>
2025-12-10 09:20:28 +0700

    Remove CORS configuration from socket.io
    Removed socket.io configuration for CORS.

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.get("/", (req, res) => {
 // Render requires this to start on the right port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "https://pila.onrender.com",
-        methods: ["GET", "POST"]
+
     }
 });
